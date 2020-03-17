105b7f034193d588a5a43b113c69e22d86da7a22
Harald Lazardzig <harald.lazardzig@gmail.com>
2020-03-17 14:54:56 +0100

    updated to latest version of minimist

--- utils/googleMapsDistance.js
+++ utils/googleMapsDistance.js
@@ -62,7 +62,7 @@ module.exports = async (addresses, myCBFunction) => {
     console.log('latlong[0] of ' + origin + ' is ' + JSON.stringify(latlong[0]))
     console.log('latlong[1] of ' + destination + ' is ' + JSON.stringify(latlong[1]))
     dist = PythagorasEquirectangular(latlong[0], latlong[1])
-    console.log('Linear distance flies is ' + dist.toFixed(2) + ' km')
+    console.log('Linear distance is ' + dist.toFixed(2) + ' km')
     console.log('and distance by car ' + distanceByCar.text)
   }
 
